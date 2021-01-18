4239f889e9a1821dc6035e02f70eacf0277f050b
arif93sulaiman <arifhip1993@yahoo.com>
2021-01-18 23:06:07 +0800

    mongodb connection

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const express = require('express')
 const expressLayouts = require('express-ejs-layouts')
+const mongoose = require('mongoose')
 
 const app = express()
 
+// db config
+const db = require('./config/keys').MongoURI
+
+//connect to db
+mongoose.connect(db, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => console.log('Mongo DB Connected'))
+    .catch(err => console.log(err))
 //EJS
 app.use(expressLayouts)
 app.set('view engine', 'ejs')
---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    MongoURI:"mongodb+srv://arif123:arif123@authentication.xbvpv.mongodb.net/<dbname>?retryWrites=true&w=majority"
+}
