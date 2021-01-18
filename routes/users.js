82194df23848347b8a6ebc31e23dec2e3adc3eba
arif93sulaiman <arifhip1993@yahoo.com>
2021-01-18 12:20:18 +0800

    view templates

--- routes/users.js
+++ routes/users.js
@@ -2,11 +2,11 @@ const express = require('express')
 const router = express.Router()
 //login
 router.get('/login', (req, res) => {
-    res.send('Login')
+    res.render('login')
 })
 //register
 router.get('/register', (req, res) => {
-    res.send('Register')
+    res.render('register')
 })
 
 
