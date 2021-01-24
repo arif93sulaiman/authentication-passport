03325edf4bef0a7f08c4c08ec598ad2772d70343
arif93sulaiman <arifhip1993@yahoo.com>
2021-01-24 16:45:26 +0800

    authentication fixed

--- config/passport.js
+++ config/passport.js
@@ -1,33 +1,37 @@
 const LocalStrategy = require('passport-local').Strategy
+var passport = require('passport');
 const mongoose = require('mongoose')
 const bcrypt = require('bcryptjs')
-const passport = require('passport')
+
 
 
 //load User model
 const User = require('../models/Users')
 
 module.exports = function(passport){
-    new LocalStrategy({ usernameField: 'email'}, (email, password, done) => {
-        //match user
-        User.findOne({ email: email})
-            .then(user => {
-                if (!user) {
-                    return done(null, false, {message: 'That email is not registered'})
-                }
-
-                //match password
-                bcrypt.compare(password, user.password, (err, isMatch) => {
-                    if (err) throw err
-                    if (isMatch) {
-                        return done(null, user)
-                    } else {
-                        return done(null, false, {message: 'password incorrect'})
+    passport.use(
+        new LocalStrategy({ usernameField: 'email'}, (email, password, done) => {
+            //match user
+            User.findOne({ email: email})
+                .then(user => {
+                    if (!user) {
+                        return done(null, false, {message: 'That email is not registered'})
                     }
+    
+                    //match password
+                    bcrypt.compare(password, user.password, (err, isMatch) => {
+                        if (err) throw err
+                        if (isMatch) {
+                            return done(null, user)
+                        } else {
+                            return done(null, false, {message: 'password incorrect'})
+                        }
+                    })
                 })
-            })
-            .catch(err => console.log(err))
-    })
+                .catch(err => console.log(err))
+        })
+
+    )
 
     passport.serializeUser((user, done) => {
         done(null, user.id);
